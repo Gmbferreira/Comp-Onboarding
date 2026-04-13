e6b22a284d5fcc181b9bb5c7c9150d8d5a691d43
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-04-13 10:33:29 -0300

    Adição da rota de Delete do Prato e mudança dos métodos put para patch nas rotas ativar e desativar.

--- back-end/demo/src/main/java/com/saboremagia/controller/PratoController.java
+++ back-end/demo/src/main/java/com/saboremagia/controller/PratoController.java
@@ -1,14 +1,23 @@
 package com.saboremagia.controller;
 
-import com.saboremagia.model.Prato;
-import com.saboremagia.repository.PratoRepository;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.util.ReflectionUtils;
-import java.lang.reflect.Field;
-import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saboremagia.model.Prato;
+import com.saboremagia.repository.PratoRepository;
 
 
 
@@ -28,12 +37,14 @@ public class PratoController {
         return pratoRepository.save(prato);
     }
 
-    @PutMapping("/{id}/ativar")
+    @PatchMapping("/{id}/ativar")
     public Prato ativarPrato(@PathVariable int id){
         Prato prato = pratoRepository.findById(id).orElseThrow();
         prato.ativarPrato();
         return pratoRepository.save(prato);
     }
+
+    @PatchMapping("/{id}/desativar")
     public Prato desativarPrato(@PathVariable int id){
         Prato prato = pratoRepository.findById(id).orElseThrow();
         prato.desativarPrato();
@@ -63,6 +74,17 @@ public class PratoController {
             return ResponseEntity.ok(atualizado);
             
         }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> apagarPrato(@PathVariable int id) {
+
+        if (!pratoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        pratoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
     
 }
