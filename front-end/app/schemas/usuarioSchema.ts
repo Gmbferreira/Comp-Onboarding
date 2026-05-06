7ad3bbd55f47cd32dacb54a298b7abe1b5e209fa
Guilherme Brina <gmbferreira12@gmail.com>
2026-05-05 22:04:38 -0300

    ajustes de token e testes

--- front-end/app/schemas/usuarioSchema.ts
+++ front-end/app/schemas/usuarioSchema.ts
@@ -1,31 +1,17 @@
 import * as z from "zod";
 
-export interface Usuario {
-  id?: number;
-  nome: string;
-  email: string;
-  senha?: string;
-  telefone: string;
-}
-
-export interface Cliente extends Usuario {
-  endereco?: string;
-}
-
-export interface LoginDTO {
-  email: string;
-  senha: string;
-}
-
 export const usuarioBaseSchema = z.object({
   id: z.number().optional(),
   nome: z.string().min(3, "O nome deve ter no mínimo 3 caracteres"),
   email: z.string().email("E-mail inválido"),
   senha: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
-  telefone: z.string().min(10, "Telefone inválido (mínimo 10 dígitos)"),
+  telefone: z.string().min(10, "Telefone inválido"),
   endereco: z.string().optional(),
 });
 
+export type Usuario = z.infer<typeof usuarioBaseSchema>;
+
+export type Cliente = Usuario; 
 
 export const cadastroSchema = usuarioBaseSchema.pick({
   nome: true,
@@ -33,12 +19,10 @@ export const cadastroSchema = usuarioBaseSchema.pick({
   telefone: true,
   senha: true,
 });
-
 export type CadastroData = z.infer<typeof cadastroSchema>;
 
 export const loginSchema = usuarioBaseSchema.pick({
   email: true,
   senha: true,
 });
-
 export type LoginData = z.infer<typeof loginSchema>;
--- front-end/app/usuario-cadastro/page.tsx
+++ front-end/app/usuario-cadastro/page.tsx
@@ -6,14 +6,14 @@ import CadastroForm from "./form";
 export default function CadastroPage() {
   return (
     <main className="min-h-screen flex flex-col md:flex-row bg-[#F5F5ED]">
-      {/* LADO ESQUERDO: INFO (Reutilizado) */}
+      
       <AuthWelcomeSide 
         linkText="Já tem uma conta?" 
         linkHref="/usuario-login" 
         linkActionText="Clique aqui" 
       />
 
-      {/* LADO DIREITO: FORMULÁRIO */}
+      
       <div className="w-full md:w-1/2 bg-[#D1E7D3] md:rounded-l-[3rem] flex items-center justify-center p-8">
         <CadastroForm />
       </div>
